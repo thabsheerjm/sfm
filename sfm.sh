ca5579fdc0d9e73b338eb48a27e134d62e6d6a1a
thabsheerjm <thabsheerjm1@gmail.com>
2024-09-24 22:49:02 -0400

    update sfm file

--- sfm.sh
+++ sfm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 # Defin vriables
+DATA_NAME="South Building"
 IMAGE_DIR="data/images/"
 OUTPUT_DIR="results/pointcloud/"
 FOCAL_LENGTH=2559.68
@@ -17,6 +18,7 @@ HARRIS_THRESHOLD=1e6
 #Run the main.py
 python3 src/main.py \
     --image_dir "$IMAGE_DIR"\
+    --data_name "$DATA_NAME"\
     --output_dir "$OUTPUT_DIR"\
     --focal_length "$FOCAL_LENGTH"\
     --principal_point "$PRINCIPAL_POINT_X" "$PRINCIPAL_POINT_Y"\
